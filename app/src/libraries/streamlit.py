aa009099454eba374057e7e4c7bad23498fe595e
Simon Peck <simon@dataengineers.co.nz>
2023-11-02 13:28:47 +1300

    3

--- app/src/libraries/streamlit.py
+++ app/src/libraries/streamlit.py
@@ -9,7 +9,7 @@ from sys import exit
 st.set_page_config(layout="wide")
 session = get_active_session()
 
-def load_app(orders_table,site_recovery_table):
+def load_app():
    
     if st.button('Create Network Rule'):
          df_network_func = session.sql(f"call app_instance_schema.create_network_rule()").collect()
@@ -20,25 +20,6 @@ def load_app(orders_table,site_recovery_table):
     if st.button('Create Secret'):
          df_secret_func = session.sql(f"call app_instance_schema.create_network_secret()").collect()
          st.write(df_secret_func)
-    else:
-        st.write('Goodbye')
-
-       
-    if st.button('Send to Monitorial'):
-         df_call_ext_func = session.sql(f"call app_instance_schema.call_monitorial_dispatch()").collect()
-         st.write(df_call_ext_func)
-    else:
-        st.write('Goodbye')
-
-    if st.button('Create Network Rule'):
-         df_net_func = session.sql(f"call app_instance_schema.create_network_rule()").collect()
-         st.write(df_net_func)
-    else:
-        st.write('Goodbye')
-
-    if st.button('Create Network Policy'):
-         df_net_func = session.sql(f"call app_instance_schema.create_network_policy()").collect()
-         st.write(df_net_func)
     else:
         st.write('Goodbye')
 
--- app/src/scripts/setup.sql
+++ app/src/scripts/setup.sql
@@ -12,7 +12,6 @@ CREATE STAGE app_code.app_jars;
 
 -- Create Streamlit app
 create or replace streamlit app_instance_schema.streamlit from '/libraries' main_file='streamlit.py';
-
 create or replace procedure app_instance_schema.create_network_rule()
 returns string
 language sql
@@ -21,7 +20,7 @@ BEGIN
    CREATE OR REPLACE NETWORK RULE external_database_network_rule
       TYPE = HOST_PORT
       VALUE_LIST = ('omnata-sandpit.database.windows.net:1433') 
-      MODE= EGRESS
+      MODE= EGRESS;
    grant usage on network rule external_database_network_rule to application role app_instance_role;
    RETURN 'created network Rule';
 END;
@@ -53,101 +52,6 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE FUNCTION READ_JDBC(OPTION OBJECT, query STRING) 
-  RETURNS TABLE(data OBJECT)
-  LANGUAGE JAVA
-  RUNTIME_VERSION = '11'
-  IMPORTS = ('/scripts/libraries/jar/mssql-jdbc-12.4.2.jre11.jar')
-  EXTERNAL_ACCESS_INTEGRATIONS = (external_database_network_rule_ext_int)
-  SECRETS = ('cred' = external_database_cred )
-  HANDLER = 'JdbcDataReader'
-AS $$
-import java.sql.*;
-import java.util.*;
-import java.util.stream.Stream;
-import com.snowflake.snowpark_java.types.SnowflakeSecrets;
-
-public class JdbcDataReader {
-
-    public static class OutputRow {
-        public Map<String, String> data;
-
-        public OutputRow(Map<String, String> data) {
-            this.data = data;
-        }
-    }
-
-    public static Class getOutputClass() {
-      return OutputRow.class;
-    }
-
-    public Stream<OutputRow> process(Map<String, String> jdbcConfig, String query) {
-        String jdbcUrl = jdbcConfig.get("url");
-        String username;
-        String password;
-        
-        if ("true".equals(jdbcConfig.get("use_secrets")))
-        {
-            SnowflakeSecrets sfSecrets = SnowflakeSecrets.newInstance();
-            var secret = sfSecrets.getUsernamePassword("cred");
-            username   = secret.getUsername();
-            password   = secret.getPassword();
-        }
-        else 
-        {
-            username = jdbcConfig.get("username");
-            password = jdbcConfig.get("password");
-        }
-        try {
-            // Load the JDBC driver 
-            Class.forName(jdbcConfig.get("driver"));
-            // Create a connection to the database
-            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-            // Create a statement for executing SQL queries
-            Statement statement = connection.createStatement();
-            // Execute the query
-            ResultSet resultSet = statement.executeQuery(query);
-            // Get metadata about the result set
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            // Create a list of column names
-            List<String> columnNames = new ArrayList<>();
-            int columnCount = metaData.getColumnCount();
-            for (int i = 1; i <= columnCount; i++) {
-                columnNames.add(metaData.getColumnName(i));
-            }
-            // Convert the ResultSet to a Stream of OutputRow objects
-            Stream<OutputRow> resultStream = Stream.generate(() -> {
-                try {
-                    if (resultSet.next()) {
-                        Map<String, String> rowMap = new HashMap<>();
-                        for (String columnName : columnNames) {
-                            String columnValue = resultSet.getString(columnName);
-                            rowMap.put(columnName, columnValue);
-                        }
-                        return new OutputRow(rowMap);
-                    } else {
-                        // Close resources
-                        resultSet.close();
-                        statement.close();
-                        connection.close();                        
-                        return null;
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }).takeWhile(Objects::nonNull);
-            return resultStream;
-        } catch (Exception e) {
-            e.printStackTrace();
-            Map<String, String> rowMap = new HashMap<>();
-            rowMap.put("ERROR",e.toString());
-            return Stream.of(new OutputRow(rowMap));
-        }
-    }
-}
-$$;
-
 
 create or replace procedure app_instance_schema.update_reference(ref_name string, operation string, ref_or_alias string)
 returns string
@@ -170,7 +74,10 @@ $$;
 
 -- Grant usage and permissions on objects
 grant usage on schema app_instance_schema to application role app_instance_role;
+grant usage on schema app_code to application role app_instance_role;
 grant usage on streamlit app_instance_schema.streamlit to application role app_instance_role;
 grant usage on procedure app_instance_schema.create_network_rule() to application role app_instance_role;
 grant usage on procedure app_instance_schema.create_network_secret() to application role app_instance_role;
-grant usage on procedure app_instance_schema.call_monitorial_dispatch() to application role app_instance_role;
+--grant usage on procedure app_instance_schema.create_read_jdbc() to application role app_instance_role;
+--grant usage on secret app_instance_schema.external_database_cred to application role app_instance_role;
+--grant usage on network rule app_instance_schema.external_database_network_rule to application role app_instance_role;
