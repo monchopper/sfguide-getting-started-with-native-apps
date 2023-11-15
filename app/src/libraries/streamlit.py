1c516743b18da65fd35ed5836ed1945b2a1fec5f
Simon Peck <simon@dataengineers.co.nz>
2023-11-15 14:28:31 +1300

    up to create not int

--- app/src/libraries/streamlit.py
+++ app/src/libraries/streamlit.py
@@ -4,7 +4,7 @@ import altair as alt
 import snowflake.snowpark as snowpark
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import count_distinct,col,sum,lit,object_construct,any_value
-import snowflake.permissions as permission
+import snowflake.permissions as permissions
 from sys import exit
 
 
@@ -12,30 +12,75 @@ st.set_page_config(layout="wide")
 session = get_active_session()
 
 def load_app():
-   
-    if st.button('Step 1.  Create Network Rule'):
-         df_network_func = session.sql(f"call monitorial_config.create_network_rule()").collect()
-         st.write(df_network_func)
-    else:
-        st.write('Network Rule not created')
 
+    df_get_account_details = session.sql(f"select CURRENT_ORGANIZATION_NAME() as organization_name,current_account_name() AS account_name,current_account() AS account_locator,current_region() AS account_region").collect()
+    st.write(df_get_account_details)
+   
+    df_network_func = session.sql(f"call monitorial_config.create_network_rule()").collect()
+    st.write(df_network_func)
+    st.write('Network Rule created')
+
+    df_create_database = session.sql(f"call monitorial_config.deploy_setup_monitorial_db_and_assest_creation_sproc()").collect()
+    st.write(df_create_database)
+    st.write('Monitorial database and sprocs not created')
 
     st.code(f"""
-        -- Step 2.  use role ACCOUNTADMIN
-        CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION monitorial_access_integration
-        ALLOWED_NETWORK_RULES = (monitorial_app.monitorial_config.monitorial_service_network_rule)
-        ENABLED = true;
-        GRANT USAGE ON INTEGRATION monitorial_access_integration TO APPLICATION MONITORIAL_APP_2;
+        -- Copy this code and run it in Snowsight under the accountadmin role
+        use role accountadmin;
+        grant ownership on procedure  montorial_db.monitorial_assets.run_setup_external_access_integrations() TO ROLE accountadmin;
+        call montorial_db.monitorial_assets.run_setup_external_access_integrations();
         """,language='sql')
     
+    #as of now.  The External network access has been created.  Need to call the func that ( the aws stuff and create the 
+    #notification integration
+
+
     
+    if st.button('Create Get ARN-AWS details from Monitorial'):
+        df_create_get_monitorial_aws_arn = session.sql(f"call create_monitorial_get_aws_arn()").collect()
+        st.write(df_create_get_monitorial_aws_arn)
+    else:
+        st.write('Monitorial get aws arn func failed')
+    
+    st.code(f"""
+        EXECUTE IMMEDIATE $$
+        DECLARE 
+            res RESULTSET DEFAULT (DESCRIBE INTEGRATION MONITORIAL_ERROR_INTEGRATION);
+            query varchar default 'select object_construct(*):property_value::string as sf_aws_external_id from table(result_scan(last_query_id())) where "property" = ?';
+                query_create_not_int default 'create notification integration if not exists MONITORIAL_ERROR_INTEGRATION
+                enabled = true
+                type = QUEUE
+                direction = OUTBOUND
+                notification_provider = AWS_SNS
+                aws_sns_topic_arn = "arn:aws:sns:ap-southeast-2:415570042924:2d59d83f-5855-4a53-b891-a843bf558cbc-notifications"
+                aws_sns_role_arn = "arn:aws:iam::415570042924:role/2d59d83f-5855-4a53-b891-a843bf558cbc-role-sns"';
+            notification_propery varchar default 'SF_AWS_EXTERNAL_ID';
+            res_integrations resultset;
+        BEGIN 
+            EXECUTE IMMEDIATE query_create_not_int;
+            DESC INTEGRATION MONITORIAL_ERROR_INTEGRATION;
+            res_integrations := (EXECUTE IMMEDIATE :query USING (notification_propery));
+        RETURN table(res_integrations);
+        END;
+        $$
+        ;
+""",language='sql')
+
+   
+
     if st.button('Step 3.  Create Monitorial Dispatch Function'):
          df_dispatch_func = session.sql(f"call monitorial_config.create_monitorial_dispatch_func()").collect()
          st.write(df_dispatch_func)
     else:
         st.write('Monitorial Dispatch Function not created')
 
+    if st.button('Step 88 Show Tasks'):
+         df_show_tasks = session.sql(f"show tasks in account").collect()
+         st.write(df_show_tasks)
+    else:
+        st.write('Cannot show tasks')
 
+    """
     if st.button('Step 4.  Grant Usage on Monitorial Dispatch Function'):
          df_dispatch_func_grant = session.sql(f"call monitorial_config.grant_monitorial_dispatch_func()").collect()
          st.write(df_dispatch_func_grant)
@@ -49,51 +94,30 @@ def load_app():
     else:
         st.write('Monitorial Dispatch Function Failed')
 
-    if st.button('Step test create warehouse for tasks.'):
+    if st.button('Test create warehouse for tasks.'):
          df_create_warehouse = session.sql(f"call monitorial_config.create_task_warehouse()").collect()
          st.write(df_create_warehouse)
     else:
-        st.write('Monitorial Dispatch Function Failed') 
+        st.write('Test create warehouse for tasks') 
 
-    if st.button('Step test task creation.  Test Creating Task'):
+    if st.button('Test Creating Monitorial like Task'):
          df_call_dispatch_func = session.sql(f"call monitorial_config.create_task_test()").collect()
          st.write(df_call_dispatch_func)
     else:
-        st.write('Monitorial Dispatch Function Failed')
+        st.write('Test create task')
 
-    if st.button('Step Manually Execute Task'):
+    if st.button('Step Test Task'):
          df = session.sql('''EXECUTE TASK monitorial_assets.t1''').collect()
          st.write(df)
     else:
-        st.write('Monitorial Dispatch Function Failed')
+        st.write('Test task works')
 
-    if st.button('Step test Get Snowflake details'):
-         df_call_account_details = session.sql(f"select CURRENT_ORGANIZATION_NAME() as organization_name,current_account_name() AS account_name,current_account() AS account_locator,current_region() AS account_region").collect()
-         st.write(df_call_account_details)
-    else:
-        st.write('Monitorial Dispatch Function Failed')
-
-
-    st.code(f"""
-        -- Step 6.  use role ACCOUNTADMIN
-        create notification integration if not exists MONITORIAL_ERROR_INTEGRATION
-            enabled = true
-            type = QUEUE
-            direction = OUTBOUND
-            notification_provider = AWS_SNS
-            aws_sns_topic_arn = 'arn:aws:sns:ap-southeast-2:415570042924:2d59d83f-5855-4a53-b891-a843bf558cbc-notifications'
-            aws_sns_role_arn = 'arn:aws:iam::415570042924:role/2d59d83f-5855-4a53-b891-a843bf558cbc-role-sns';
-        grant usage on integration MONITORIAL_ERROR_INTEGRATION to role SYSADMIN;
-        grant usage on integration MONITORIAL_ERROR_INTEGRATION TO APPLICATION MONITORIAL_APP_2;
-        """,language='sql')
-    
-    if st.button('Step Get Notification Details. '):
-         df = session.sql('''desc integration MONITORIAL_ERROR_INTEGRATION''').collect()
-         st.write(df)
-    else:
-        st.write('Monitorial Dispatch Function Failed')
+    """
 
 load_app()
+
+if not permissions.get_held_account_privileges(["CREATE DATABASE"]):
+    st.error("The app needs CREATE DB privilege to replicate data")
 
     
 
--- app/src/scripts/setup.sql
+++ app/src/scripts/setup.sql
@@ -7,21 +7,80 @@ create application role if not exists monitorial_admin;
 create or alter versioned schema monitorial_config;
 create or replace schema monitorial_assets;
 
---CREATE OR REPLACE WAREHOUSE p_task_wh WAREHOUSE_SIZE='XSMALL' INITIALLY_SUSPENDED=TRUE auto_suspend = 60 auto_resume = true;
--- Create Streamlit app
-create or replace streamlit monitorial_config.streamlit from '/libraries' main_file='streamlit.py';
+CREATE TABLE IF NOT EXISTS monitorial_assets.Configs (
+   setting object
+);
 
-create or replace procedure monitorial_config.create_task_warehouse()
+
+
+create or replace procedure monitorial_config.create_monitorial_get_aws_arn()
 returns string
 language sql
-AS $$
+AS '
 BEGIN
-   CREATE OR REPLACE WAREHOUSE p_task_wh WAREHOUSE_SIZE=XSMALL INITIALLY_SUSPENDED=TRUE auto_suspend = 60 auto_resume = true;
-   grant usage on warehouse p_task_wh to application role monitorial_admin;
-   grant operate on warehouse p_task_wh to application role monitorial_admin;
-   RETURN 'created warehouse';
-END;
+CREATE OR REPLACE FUNCTION get_monitorial_get_aws_arn()
+RETURNS STRING
+LANGUAGE PYTHON
+RUNTIME_VERSION = 3.9
+HANDLER = ''retrieve_arn_from_aws''
+PACKAGES = (''snowflake-snowpark-python'',''requests'')
+AS
+$$
+import _snowflake
+import json
+import requests
+import uuid
+
+session = requests.Session()
+
+def retrieve_arn_from_aws():
+   return ["arn:aws:sns:ap-southeast-2:415570042924:2d59d83f-5855-4a53-b891-a843bf558cbc-notifications|arn:aws:iam::415570042924:role/2d59d83f-5855-4a53-b891-a843bf558cbc-role-sns"]
+
 $$;
+END;
+';
+
+
+create or replace procedure monitorial_config.deploy_setup_monitorial_db_and_assest_creation_sproc()
+returns string
+language sql
+AS '
+BEGIN
+   create or replace database montorial_db;
+   create or replace schema montorial_db.monitorial_assets;
+   grant usage on database montorial_db to application role monitorial_admin;
+   grant usage on schema montorial_db.monitorial_assets to application role monitorial_admin;
+   CREATE OR REPLACE PROCEDURE montorial_db.monitorial_assets.run_setup_external_access_integrations()
+    RETURNS VARCHAR(16777216)
+    LANGUAGE JAVASCRIPT
+    EXECUTE AS OWNER
+    AS
+    $$
+    try {
+    
+            var external_network_access_query = `
+            CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION monitorial_access_integration
+            ALLOWED_NETWORK_RULES = (monitorial_app_2.monitorial_config.monitorial_service_network_rule)
+            ENABLED = true;`
+            snowflake.createStatement({ sqlText: external_network_access_query }).execute();   
+
+            var grant_to_external_network_access_query = `
+            GRANT USAGE ON INTEGRATION monitorial_access_integration TO APPLICATION MONITORIAL_APP_2;`
+            snowflake.createStatement({ sqlText: grant_to_external_network_access_query }).execute();  
+    
+    } catch (err) {
+
+        result = `Failed Caller: Code: ${err.code} \\n  State: ${err.state}`;
+        result += `\\n  Message: ` + err.message;
+        result += `\\nStack Trace:\\n` + err.stackTraceTxt;
+        return result;
+    }
+    return ''Done'';
+    $$;
+END;
+';
+create or replace streamlit monitorial_config.streamlit from '/libraries' main_file='streamlit.py';
+
 create or replace procedure monitorial_config.create_network_rule()
 returns string
 language sql
@@ -85,17 +144,28 @@ def send_notification(object_name: str):
 $$;
 END;
 ';
-create or replace procedure monitorial_config.grant_monitorial_dispatch_func()
+create or replace procedure monitorial_config.grant_func_privileges()
 returns string
 language sql
 AS $$
 BEGIN
    GRANT USAGE ON FUNCTION monitorial_dispatch(STRING) to application role monitorial_admin;
+   GRANT USAGE ON FUNCTION get_monitorial_get_aws_arn() to application role monitorial_admin;
    RETURN 'Granted monitorial_dispatch function to monitorial_admin role';
 END;
 $$;
-
-create or replace procedure monitorial_config.create_task_test(TASK_NAME string, OBJECT_NAME string)
+create or replace procedure monitorial_config.create_task_warehouse()
+returns string
+language sql
+AS $$
+BEGIN
+   CREATE OR REPLACE WAREHOUSE p_task_wh WAREHOUSE_SIZE=XSMALL INITIALLY_SUSPENDED=TRUE auto_suspend = 60 auto_resume = true;
+   grant usage on warehouse p_task_wh to application role monitorial_admin;
+   grant operate on warehouse p_task_wh to application role monitorial_admin;
+   RETURN 'created warehouse';
+END;
+$$;
+create or replace procedure monitorial_config.create_task_test()
 returns string
 language sql
 AS $$
@@ -138,10 +208,13 @@ grant create network rule on schema monitorial_config to application role monito
 grant usage on streamlit monitorial_config.streamlit to application role monitorial_admin;
 grant usage on procedure monitorial_config.create_network_rule() to application role monitorial_admin;
 grant usage on procedure monitorial_config.create_monitorial_dispatch_func() to application role monitorial_admin;
-grant usage on procedure monitorial_config.grant_monitorial_dispatch_func() to application role monitorial_admin;
+grant usage on procedure monitorial_config.grant_func_privileges() to application role monitorial_admin;
 grant usage on procedure monitorial_config.create_task_test() to application role monitorial_admin;
+grant usage on procedure monitorial_config.create_monitorial_get_aws_arn() to application role monitorial_admin;
+grant usage on procedure monitorial_config.deploy_setup_monitorial_db_and_assest_creation_sproc() to application role monitorial_admin;
 grant usage on procedure monitorial_config.create_task_warehouse() to application role monitorial_admin;
 grant create task on schema monitorial_config to application role monitorial_admin;
+GRANT SELECT ON TABLE monitorial_assets.Configs TO APPLICATION ROLE monitorial_admin;
 
 --grant usage on warehouse p_task_wh to application role monitorial_admin;
 --grant operate on warehouse p_task_wh to application role monitorial_admin;
